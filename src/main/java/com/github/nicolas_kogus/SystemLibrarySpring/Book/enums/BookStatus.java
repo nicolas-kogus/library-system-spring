ea56e378991556c31bbc6b1718ef2792f2895446
nicolas-kogus <nicolas-kogux67@gmail.com>
2026-04-24 20:03:07 -0300

    updated comments

--- src/main/java/com/github/nicolas_kogus/SystemLibrarySpring/Book/enums/BookStatus.java
+++ src/main/java/com/github/nicolas_kogus/SystemLibrarySpring/Book/enums/BookStatus.java
@@ -1,6 +1,12 @@
 package com.github.nicolas_kogus.SystemLibrarySpring.Book.enums;
 
+/**
+ * Defines the possible availability states of a book.
+ */
 public enum BookStatus {
+    /** The book is currently checked out by a user. */
     RENTED_BOOK,
+    
+    /** The book is available for borrowing. */
     BOOK_AVAILABLE
 }
